5788d21875b6ecaa05f7d665533bed8939adc25a
Diwakar.ameo <diwakar.ameo@gmail.com>
2020-10-08 16:44:11 +0530

    Refactored design and added font-family

--- src/app/pages/item-list/item-list.component.ts
+++ src/app/pages/item-list/item-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-item-list',
@@ -7,8 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemListComponent implements OnInit {
 
-  constructor() { }
+  items:any[] = [
+    {item_name:'P/663553/DY/2020+10/2020'},
+    { item_name: 'P/663553/DY/2020+10/2020' },
+    { item_name: 'P/663553/DY/2020+10/2020' },
+    { item_name: 'P/663553/DY/2020+10/2020' },
+    { item_name: 'P/663553/DY/2020+10/2020' },
+    { item_name: 'P/663553/DY/2020+10/2020' },
+    { item_name: 'P/663553/DY/2020+10/2020' },
+    { item_name: 'P/663553/DY/2020+10/2020' },
+    { item_name: 'P/663553/DY/2020+10/2020' },
+    { item_name: 'P/663553/DY/2020+10/2020' },
+    { item_name: 'P/663553/DY/2020+10/2020' },
+  ]
 
+  constructor(private navctrl: NavController) { }
+  
   ngOnInit() {}
+
+  backbtn(): void{
+   this.navctrl.navigateBack(['/home']);
+  }
 
 }
