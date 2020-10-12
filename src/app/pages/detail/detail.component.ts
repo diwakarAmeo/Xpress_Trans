cda7e8030fbc26204baa6bb6f2e4e4bd9db28d23
Diwakar.ameo <diwakar.ameo@gmail.com>
2020-10-12 16:33:38 +0530

    update detail page

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -10,15 +10,27 @@ import { HomeService } from 'src/app/services/home-service';
 export class DetailComponent implements OnInit {
 
   pickUpObject: any;
-
+  barcodeData:any[] =[];
+  listItem: any[] = [];
   constructor(
     private navctrl: NavController,
     private homeService: HomeService
   ) { 
     this.pickUpObject = this.homeService.pickUpObject;
+    this.barcodeData = this.pickUpObject['barcodedata'];
+    this.extractBarcode()
   }
 
   ngOnInit() { }
+
+
+  extractBarcode() {
+    this.barcodeData.forEach((x) => {
+       this.listItem.push(JSON.parse(x));
+    });
+    return this.listItem;
+  }
+
 
   backbtn(): void {
     this.navctrl.navigateBack(['/home']);
