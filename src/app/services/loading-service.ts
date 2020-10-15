ab7bd48593188a0098d0df351b4e4bbaed81edb6
Diwakar.ameo <diwakar.ameo@gmail.com>
2020-10-15 16:00:55 +0530

    Refctored Loader

--- src/app/services/loading-service.ts
+++ src/app/services/loading-service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NgZone } from '@angular/core';
+import { Injectable } from '@angular/core';
 import {
     HttpRequest,
     HttpHandler,
@@ -7,29 +7,21 @@ import {
 } from '@angular/common/http';
 import { finalize, catchError } from 'rxjs/operators';
 import { HelperService } from './helper-service';
-import { NetworkStatus, PluginListenerHandle, Plugins } from '@capacitor/core';
-import { Observable, BehaviorSubject } from 'rxjs';
-const { Network } = Plugins;
+
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
-    networkStatus: NetworkStatus;
-    networkListener: PluginListenerHandle;
-    isOnline$: Observable<boolean>;
-    private statusSubject = new BehaviorSubject<boolean>(false);
+
     public totalRequests = 0;
 
     constructor(
-        private helperService: HelperService,
-        private zone: NgZone) { }
+        private helperService: HelperService) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler):
         import("rxjs").Observable<HttpEvent<any>> {
         this.totalRequests++;
         if (this.totalRequests === 1) {
-            this.networkHandler();
             this.helperService.isLoading = true;
         } else {
-            this.networkHandler();
             this.helperService.isLoading = false;
         }
         return <any>next.handle(req).pipe(
@@ -40,7 +32,6 @@ export class LoadingInterceptor implements HttpInterceptor {
             finalize(() => {
                 this.totalRequests--;
                 if (this.totalRequests === 0) {
-                    // this.networkHandler();
                     this.helperService.isLoading = false;
                 } else {
                     this.helperService.isLoading = true;
@@ -48,19 +39,5 @@ export class LoadingInterceptor implements HttpInterceptor {
             })
         )
     }
-
-    async networkHandler() {
-        
-        this.networkListener = Network.addListener('networkStatusChange', (status) => {
-            this.zone.run(() => {
-                console.log("Network status changed", status);
-                this.networkStatus = status;
-            });
-        });
-
-        this.networkStatus = await Network.getStatus();
-        console.log(this.networkStatus);
-    }
-    
 
 }
