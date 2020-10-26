59dd357e79b8d8d5aec601ba15030c7bdc0a3bcb
Diwakar.ameo <diwakar.ameo@gmail.com>
2020-10-26 17:00:50 +0530

    Refactored response design

--- src/app/pages/response/response.page.ts
+++ src/app/pages/response/response.page.ts
@@ -10,12 +10,19 @@ import { NavController } from '@ionic/angular';
 export class ResponsePage implements OnInit {
 
   responseData: any[] = [];
-
+  sortedData: any[] = [];
+  defaultValues: any[] = [];
   constructor(private route: ActivatedRoute, private router: Router, private navCtrl: NavController) {
     this.route.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation().extras.state) {
-        this.responseData = this.router.getCurrentNavigation().extras.state.data;
-        console.log(this.responseData);
+        this.responseData.push(this.router.getCurrentNavigation().extras.state.data);
+        console.log(this.responseData, this.responseData.length);
+        for (let index = 0; index < this.responseData.length; index++) {
+          
+          let indexStr = index.toString();
+          this.sortedData = JSON.parse(this.responseData[index][indexStr]);
+          console.log(this.sortedData);
+         }
         
       } 
       else {
